bb81610ccf3c8f99fc3ebae30b85f4be793ad104
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-06-27 18:56:34 +0530

    chore: include the routers fastapi reference

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 
+from routes.students import router as  students_router
+from routes.teachers import router as  teachers_router
+
+
 app = FastAPI(docs_url="/swagger", redoc_url="/documentation")
 
 @app.get("/")
@@ -9,3 +13,8 @@ def home():
 @app.get("/about")
 def about():
     return {"message": "this message is from about page"}
+
+
+#include routers
+app.include_router(students_router)
+app.include_router(teachers_router)
