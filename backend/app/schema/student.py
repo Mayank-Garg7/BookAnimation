a60aafddf5ea32550bbd15d5add55bd2d602b570
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-07-01 16:59:58 +0530

    chore: add schema to update and response

--- backend/app/schema/student.py
+++ backend/app/schema/student.py
@@ -1,12 +1,24 @@
-from pydantic import BaseModel, Field, EmailStr, HttpUrl
-
-class createStudent (BaseModel):
-    roll_number: int | None = None # this is not required
-    name: str = Field(min_length=5, max_length=20, description="Students full name")
-    age: int = Field(ge = 18, le = 60)
-    department: str = "CSE" # default value for department
-    email: EmailStr
-    website: HttpUrl
+from pydantic import BaseModel, Field, EmailStr
 
 
+
+class studentBase(BaseModel):
+    name: str = Field(min_length=5, max_length=30, description="full name", examples="Mayank Garg") 
+    age: int = Field(ge=18, le=60)
+    email: EmailStr 
+    department: str
+
+class createStudent (studentBase):
+    pass
+
+class updateStudent (BaseModel):
+    name: str | None = Field(default=None, min_length=5, max_length=30, description="full name", examples="Mayank Garg") 
+    age: int | None = Field(default=None, ge=18, le=60) 
+    email: EmailStr | None = None
+    department: str | None = None
+    
+
+class responseStudent(BaseModel):
+    id: int
+    isActive: bool
 
